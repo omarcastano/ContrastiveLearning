a82d94ba688379d0a8b47ac5966379e472b61fb2
omarcastano <56597125+omarcastano@users.noreply.github.com>
2021-10-23 17:27:44 -0500

    Create Encoders.py

---
+++ Encoders.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+from ResNet import ResNet18
+
+def get_vgg_encoder(dropout=None, input_shape):
+    encoder = tf.keras.models.Sequential([
+        tf.keras.layers.Input(shape=input_shape) ,
+        
+        tf.keras.layers.Conv2D(32, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_normal'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(32, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_normal'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(32, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_normal'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.Conv2D(64, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_normal'), 
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(64, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_normal'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(64, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_normal'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.Conv2D(128, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_normal'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(128, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_normal'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(128, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_normal'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPool2D(), 
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.GlobalAveragePooling2D()
+    ], name='Encoder')
+    return encoder
+  
+  
+def get_resnet18_encoder(input_shape):
+    encoder = tf.keras.models.Sequential([
+        ResNet18(input_shape)
+    ], name='Encoder')
+    return encoder
