3e7e44b8596651fe3049e6a8916d3f01268d86df
{omarcastano} <omar.castano@udea.edu.co>
2021-11-18 00:14:07 +0000

    change ann

--- model_util.py
+++ model_util.py
@@ -128,7 +128,7 @@ def ANN_test(ds_train, ds_test, input_shape, encoder, batch_size, epochs):
             tf.keras.layers.Dropout(0.2),
             tf.keras.layers.Dense(10, activation='softmax')
     ])
-    nadam = tf.keras.optimizers.Nadam(learning_rate=0.0005)
+    nadam = tf.keras.optimizers.Nadam(learning_rate=0.0001)
     model.compile(optimizer=nadam, loss="sparse_categorical_crossentropy", metrics = "acc")
     callback = tf.keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)
     model.fit(data_train_labeled, batch_size=batch_size, epochs=epochs, validation_data=data_test_labeled, callbacks=[callback], verbose=0)
